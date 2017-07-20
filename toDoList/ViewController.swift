2619648e00bbc50ae35c9ed17ac59959540f25a9
Max Walters <waltm3@mail.broward.edu>
2017-07-20 13:34:35 -0400

    Added delete buttons and clear all button.

--- toDoList/ViewController.swift
+++ toDoList/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController, UITextViewDelegate{
     //elements
     @IBOutlet weak var addTxt: UITextField!
     @IBOutlet weak var listTv: UITextView!
+    @IBOutlet weak var error: UILabel!
+    
+    //list 
+    //List
+    var items:[String] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,21 +42,79 @@ class ViewController: UIViewController, UITextViewDelegate{
        Add(self)
         return false
     }
-
-    //List f
-    var items:[String] = []
     
+    //deletes first index
+    @IBAction func del1(_ sender: Any) {
+        if (items.isEmpty){
+            error.text = "Nothing to delete"
+            return
+        }
+        
+        error.text = ""
+        //clears output so it appears to only add
+        listTv.text = ""
+        
+        //remove last item
+        items.remove(at: 0)
+        for item in items{
+            listTv.text.append("\(item)\n")
+        }
+       
+    }
+    
+    //deletes second index
+    @IBAction func del2(_ sender: Any) {
+        if (items[1].isEmpty){
+            error.text = "Nothing to delete at this index"
+            return
+        }
+        
+        error.text = ""
+        //clears output so it appears to only add
+        listTv.text = ""
+        
+        //remove last item
+        items.remove(at: 1)
+        for item in items{
+            listTv.text.append("\(item)\n")
+            
+        }
+    }
+    
+    //CLears whole list
+    @IBAction func delAll(_ sender: Any) {
+        if (items.isEmpty){
+            error.text = "List already clear"
+            return
+        }
+        
+        error.text = ""
+        
+        //clears output
+        listTv.text = ""
+        
+        items.removeAll()
+        
+        for item in items{
+            listTv.text.append("\(item)\n")
+        }
+        
+    }
+    
+    //Adds one item to list
     @IBAction func Add(_ sender: Any) {
         
         //checks if the field is empty
         if (addTxt.text! == ""){
+            error.text = "Add What?"
             return
         }
-        
+        error.text = ""
+
         //appends text to an item
         items.append(addTxt.text!)
         
-        //clears input
+        //clears output so it appears to only add
         listTv.text = ""
         
         //for loop to add to text view
@@ -64,5 +128,10 @@ class ViewController: UIViewController, UITextViewDelegate{
         addTxt.resignFirstResponder()
     
     }
+    
+    //Some rules for fun in future maybe
+    
+    //Show delete button(s) only if there is something to delete
+    
 }
 
